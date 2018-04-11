857bfe031963e46309a6cd79b0d05822a9b9dccf
Alexander Ryazanov <alryaz@xavux.com>
2018-04-11 09:54:08 +0300

    Revert "More robust bootstrapping for rapid deployment"
    This reverts commit 7149f741e742f72059b38fe32bf0a7687bdc0c44.

--- scripts/defaults.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-[ -z "$ENV" ] && ENV=production
-[ -z "$SERVER_DIR" ] && SERVER_DIR="$( dirname $( dirname $( realpath ${BASH_SOURCE} ) ) )"
-[ -z "$SERVER_PORT" ] && SERVER_PORT=3000
